b7e706d5023529214b09ab5a0704a6a6e82c86c7
unknown <asiac00@gmail.com>
2017-11-05 16:52:37 +0100

    fix task 6

--- Zadanie 9.6/js/scripts.js
+++ Zadanie 9.6/js/scripts.js
@@ -1,18 +1,3 @@
-var count = 0;
-
-var list = document.getElementById('list'),
-    add = document.getElementById('addElem');
-
-add.addEventListener('click', function(e) {
-  var element = document.createElement('li');
-  
-  element.innerHTML = 'item ' + (++count);
- 
-  list.appendChild(element);
-  
-});
-
-/*
 var list = document.getElementById('list'),
     add = document.getElementById('addElem');
 
@@ -20,10 +5,9 @@ add.addEventListener('click', function(e) {
   var element = document.createElement('li');
   
   var li = document.getElementsByTagName('li');
-  li.length
   
   element.innerHTML = 'item ' + li.length;
  
   list.appendChild(element);
   
-});*/
+});
